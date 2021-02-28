15329deda0bdcd8be23fe95d206799a369e50d08
TEERMIIINAAL <76493060+TEERMIIINAAL@users.noreply.github.com>
2021-02-28 08:14:41 +0400

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -1,24 +1,16 @@
 import discord
 from discord.ext import commands
-
 f = open('members.txt', 'w')
 f.close()
-
 b = open('channels.txt', 'w')
 b.close()
-
 k = open('roles.txt', 'w')
 k.close()
-
 u = open('emojis.txt', 'w')
 u.close()
-
 TOKEN = input("Enter your bot's token: ")
-
 intents = discord.Intents.all()
 client = commands.Bot(command_prefix='2439x', intents=intents)
-
-
 @client.event
 async def on_ready():
     print(f'[SUCCESFUL]  - Logged in as {client.user}.')
@@ -37,6 +29,4 @@ async def on_ready():
     for emoji in id.emojis:
         em.write(f"{emoji.id}\n")
     print("\nScraped Members\nScraped Channels\nScraped Emojis\nScraped Roles\n\n[SUCCESFUL] - Server has been scraped you may close this window now.")
-
-
 client.run(TOKEN)
